8f8f180540c04f1019d538ef53210c863b826385
mads-byte <mbutl224@icloud.com>
2025-08-04 17:46:53 -0400

    add validation to name/description

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const incomeSection = document.getElementById('incomeSection');
+const expenseSection = document.getElementById('expenseSection');
+const summarySection = document.getElementById('summarySection');
+const incomeNameInput = document.getElementById('incomeName');
+const expenseNameInput = document.getElementById('expenseName');
+const warning = document.getElementById('warning');
+
+function validateName() {
+    if (incomeNameInput.value.length < 1) {
+        warning.textContent = "Please enter a name or description";
+    };
+    if (incomeNameInput.value.length >= 45) {
+        warning.textContent = "character limit reached";
+    }
+
+};
+
