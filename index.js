e3ee54e787a772df0f6252faac110ef62e0731a1
mads-byte <mbutl224@icloud.com>
2025-08-05 13:32:15 -0400

    finalize name input validation

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const expenseSection = document.getElementById('expenseSection');
 const summarySection = document.getElementById('summarySection');
 const incomeNameInput = document.getElementById('incomeName');
 const expenseNameInput = document.getElementById('expenseName');
+const amountInput = document.getElementById('amountInput');
 const warning = document.getElementById('warning');
+const amountWarning = document.getElementById('amountWarning');
 const warning2 = document.getElementById('warning2');
 
 
@@ -23,20 +25,34 @@ expenseNameInput.addEventListener('input', () => {
 function validateName() {
     if (incomeNameInput.value.trim() < 1) {
         warning.textContent = "Please enter a name or description";
+        return false;
     }
     else if (incomeNameInput.value.length >= 1) {
         warning.textContent = "";
+        return false;
     };
 };
 
 function validateExpenseName() {
     if (incomeNameInput.value.trim() < 1) {
         warning2.textContent = "Please enter a name or description";
+        return false;
     }
     else if (incomeNameInput.value.length >= 1) {
         warning2.textContent = "";
+        return false;
     };
 };
+
+function validateAmount() {
+    if (amountInput.value == undefined) {
+        amountWarning.textContent = "Please enter an amount";
+        return false;
+    }
+    else if (amountInput.value)
+};
+
+
 
 
 
