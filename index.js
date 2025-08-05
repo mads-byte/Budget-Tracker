f7d4e4efd0ea0f20f2452f0772150d11229ee87e
mads-byte <mbutl224@icloud.com>
2025-08-05 09:31:15 -0400

    change warning message event

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const incomeNameInput = document.getElementById('incomeName');
 const expenseNameInput = document.getElementById('expenseName');
 const warning = document.getElementById('warning');
 
+
+
+incomeNameInput.addEventListener('input', () => {
+    if (incomeNameInput.value.length >= 45) {
+        warning.textContent = "character limit reached";
+    }
+});
+
 function validateName() {
     if (incomeNameInput.value.length < 1) {
         warning.textContent = "Please enter a name or description";
     };
-    if (incomeNameInput.value.length >= 45) {
-        warning.textContent = "character limit reached";
-    }
-
 };
 
