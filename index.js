3dc532419ec25c799860b52ee6bb9a22acd7d5d8
mads-byte <mbutl224@icloud.com>
2025-08-05 18:12:09 -0400

    add appendIncome functionality

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const amountInput = document.getElementById('amountInput');
 const warning = document.getElementById('warning');
 const amountWarning = document.getElementById('amountWarning');
 const warning2 = document.getElementById('warning2');
+const firstNameInput = document.getElementById('firstNameInput');
+const lastNameInput = document.getElementById('lastNameInput');
+const userNameDisplay = document.getElementById('userNameDisplay');
+const nameSubmitBtn = document.getElementById('nameSubmit');
+const incomeList = document.getElementById('incomeList');
+const appendIncomeBtn = document.getElementById('appendIncome');
+const numRegex = /^\d+$/;
+let yourBudget;
 
 
-
-incomeNameInput.addEventListener('input', () => {
-    if (incomeNameInput.value.length >= 45) {
-        warning.textContent = "character limit reached";
-    }
-});
 
 expenseNameInput.addEventListener('input', () => {
     if (expenseNameInput.value.length >= 45) {
@@ -22,16 +24,7 @@ expenseNameInput.addEventListener('input', () => {
     }
 });
 
-function validateName() {
-    if (incomeNameInput.value.trim() < 1) {
-        warning.textContent = "Please enter a name or description";
-        return false;
-    }
-    else if (incomeNameInput.value.length >= 1) {
-        warning.textContent = "";
-        return false;
-    };
-};
+
 
 function validateExpenseName() {
     if (incomeNameInput.value.trim() < 1) {
@@ -44,13 +37,70 @@ function validateExpenseName() {
     };
 };
 
-function validateAmount() {
-    if (amountInput.value == undefined) {
-        amountWarning.textContent = "Please enter an amount";
+
+class Budget {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    appendName(firstName, lastName) {
+        userNameDisplay.textContent = `${firstName} ${lastName}`;
+    }
+};
+
+class BudgetIncome extends Budget {
+    incomeNames = [];
+    incomeAmounts = [];
+
+};
+
+nameSubmitBtn.addEventListener("click", () => {
+    let fname = firstNameInput.value;
+    let lname = lastNameInput.value;
+    yourBudget = new Budget(fname, lname);
+    yourBudget.appendName(fname, lname);
+})
+incomeNameInput.addEventListener('input', () => {
+    if (incomeNameInput.value.length >= 45) {
+        warning.textContent = "character limit reached";
+    }
+});
+
+incomeNameInput.addEventListener('blur', () => {
+    if (incomeNameInput.value.trim() < 1) {
+        warning.textContent = "Please enter a name or description";
         return false;
     }
-    else if (amountInput.value)
-};
+    else if (incomeNameInput.value.length >= 1) {
+        warning.textContent = "";
+        return true;
+    };
+});
+
+amountInput.addEventListener('blur', () => {
+    let stringAmount = amountInput.value.toString();
+    let isValid = numRegex.test(stringAmount);
+    if (isValid) {
+        amountWarning.textContent = "";
+        appendIncomeBtn.disabled = false;
+        return true;
+    }
+    else {
+        amountWarning.textContent = "Please enter a valid number";
+        appendIncomeBtn.disabled = true;
+        return false;
+    };
+})
+
+appendIncomeBtn.addEventListener("click", () => {
+    if (incomeNameInput.value.length <= 45 && incomeNameInput.value.length >= 1) {
+        const newIncome = document.createElement("li");
+        newIncome.textContent = `${incomeNameInput.value} ${amountInput.value}`;
+        incomeList.appendChild(newIncome);
+    }
+});
+
+
 
 
 
